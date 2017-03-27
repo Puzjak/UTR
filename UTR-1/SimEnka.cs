eb08e9c2878a80d377ad7a280bc5b74fd196c67a
Puzjak <puzjakmatija@gmail.com>
2017-03-27 03:34:22 +0200

    Nondeterministic finite automaton with epsilon transitions

---
+++ UTR-1/SimEnka.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace UTR_1
+{
+    public class SimEnka
+    {
+        private static IDictionary<string, State> _states = new SortedDictionary<string, State>();
+        private static State _initialState;
+        private static ISet<State> _currentStates;
+        private static ISet<State> _nextStates;
+        private static IList<string> _inputValues = new List<string>();
+        private static ISet<string> _possibleInputs = new SortedSet<string>();
+
+        public static void Main(string[] args)
+        {
+            _states.Add(State.DefaultState.Name, State.DefaultState);
+
+            //Reading input values
+            var line = Console.ReadLine();
+            line = line.Replace(" ", "");
+            foreach (var input in line.Split('|'))
+            {
+                _inputValues.Add(input);
+            }
+
+            //Reading states
+            line = Console.ReadLine();
+            line = line.Replace(" ", "");
+            var tmp = line.Split(',');
+            foreach (var state in tmp)
+            {
+                _states.Add(state, new State(state));
+            }
+
+            //Reading all possible inputs
+            line = Console.ReadLine();
+            line = line.Replace(" ", "");
+            foreach (var input in line.Split(','))
+            {
+                _possibleInputs.Add(input);
+            }
+
+            //Initializing transitions for every input to "# state"
+            foreach (var state in _states)
+            {
+                state.Value.InitializeTransitions(_possibleInputs);
+            }
+
+            //Reading acceptable states
+            line = Console.ReadLine();
+            line = line.Replace(" ", "");
+            foreach (var state in line.Split(','))
+            {
+                _states[state].IsAcceptable = true;
+            }
+
+            //Reading initial state
+            line = Console.ReadLine();
+            line = line.Replace(" ", "");
+            _initialState = _states[line];
+
+            //Reading transitions
+            while (true)
+            {
+                line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) break;
+                tmp = line.Split(new[] { "->" }, StringSplitOptions.None);
+                var initialState = tmp[0].Split(',')[0];
+                var input = tmp[0].Split(',')[1];
+                var nextStatesString = tmp[1].Split(',');
+                var nextStates = new SortedSet<State>();
+                foreach (var state in nextStatesString)
+                {
+                    nextStates.Add(_states[state]);
+                }
+                _states[initialState].AddTransition(input, nextStates);
+            }
+
+            //Solution
+            foreach (var inputValue in _inputValues)
+            {
+                var result = new StringBuilder();
+                result.Append(_initialState + "|");
+                _currentStates = new SortedSet<State> { _initialState };
+                _nextStates = new SortedSet<State>();
+                var eStates = new SortedSet<State>();
+                if (_initialState.Transitions.ContainsKey("$"))
+                {
+                    _nextStates.UnionWith(_initialState.Transitions["$"]);
+                }
+                foreach (var input in inputValue.Split(','))
+                {
+                    foreach (var state in _currentStates)
+                    {
+                        _nextStates.UnionWith(state.Transitions[input]);
+                    }
+
+                    foreach (var state in _nextStates)
+                    {
+                        if (state.Transitions.ContainsKey("$"))
+                        {
+                            eStates.UnionWith(state.GetEpsilonTransitionsIterative());
+                        }
+                    }
+                    _nextStates.UnionWith(eStates);
+
+                    if (_nextStates.Count() > 1 && _nextStates.Contains(State.DefaultState))
+                    {
+                        _nextStates.Remove(State.DefaultState);
+                    }
+
+                    foreach (var state in _nextStates)
+                    {
+                        result.Append(state + ",");
+                    }
+                    result.Remove(result.Length - 1, 1);
+                    result.Append("|");
+
+                    eStates = new SortedSet<State>();
+                    _currentStates = new SortedSet<State>();
+                    _currentStates.UnionWith(_nextStates);
+                    _nextStates = new SortedSet<State>();
+
+                }
+                result.Remove(result.Length - 1, 1);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ UTR-1/State.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UTR_1
+{
+    public class State : IComparable<State>, IEquatable<State>
+    {
+        public string Name { get; set; }
+
+        public bool IsAcceptable { get; set; }
+
+        public IDictionary<string, SortedSet<State>> Transitions { get; private set; }
+
+        public static readonly State DefaultState = new State("#");
+
+
+        public State(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name), "Name of state cannot be null");
+            }
+            Name = name;
+            IsAcceptable = false;
+            Transitions = new SortedDictionary<string, SortedSet<State>>();
+        }
+        /// <summary>
+        /// Every state of machine must be initiated for every input value the machine can take in.
+        /// After initialization transition for every input value ends in default state "#"
+        /// </summary>
+        /// <param name="inputValues">List of input values machine supports</param>
+        public void InitializeTransitions(ICollection<string> inputValues)
+        {
+            if (inputValues == null)
+            {
+                throw new ArgumentNullException(nameof(inputValues), "List of input values cannot be null");
+            }
+
+            foreach (var input in inputValues)
+            {
+                var set = new SortedSet<State>();
+                set.Add(DefaultState);
+                Transitions.Add(input, set);
+            }
+        }
+        /// <summary>
+        /// Every state of machine must be initiated for every input value the machine can take in.
+        /// After initialization transition for every input value ends in default state "#"
+        /// </summary>
+        /// <param name="inputValues">String array of input values machine supports</param>
+        public void InitializeTransitions(string[] inputValues)
+        {
+            if (inputValues == null)
+            {
+                throw new ArgumentNullException(nameof(inputValues), "Array of input values cannot be null");
+            }
+
+            foreach (var input in inputValues)
+            {
+                var set = new SortedSet<State>();
+                set.Add(DefaultState);
+                Transitions.Add(input, set);
+            }
+        }
+
+        public void AddTransition(string input, SortedSet<State> nextStates)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input), "Input value cannot be null");
+            }
+            if (nextStates == null)
+            {
+                throw new ArgumentNullException(nameof(nextStates), "Set of next states cannot be null");
+            }
+
+            Transitions.Remove(input);
+            Transitions.Add(input, nextStates);
+        }
+
+        public bool RemoveTransition(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input), "Input value cannot be null");
+            }
+            return Transitions.ContainsKey(input) && Transitions.Remove(input);
+        }
+
+        public void RemoveAllTransitions()
+        {
+            Transitions = new SortedDictionary<string, SortedSet<State>>();
+        }
+
+        public ISet<State> GetEpsilonTransitionsIterative()
+        {
+            var transitions = new SortedSet<State>();
+            var stateArrayStack = new Stack<State[]>();
+            var indexStack = new Stack<int>();
+            if (!Transitions.ContainsKey("$")) return transitions;
+            var transitionsArray = Transitions["$"].ToArray();
+            for (int i = 0; i < transitionsArray.Length; i++)
+            {
+                if (!transitions.Contains(transitionsArray[i]))
+                {
+                    transitions.Add(transitionsArray[i]);
+                    if (transitionsArray[i].Transitions.ContainsKey("$"))
+                    {
+                        indexStack.Push(i);
+                        stateArrayStack.Push(transitionsArray);
+                        transitionsArray = transitionsArray[i].Transitions["$"].ToArray();
+                        i = -1;
+                    }
+                }
+
+                if (i >= transitionsArray.Length - 1 && indexStack.Count > 0)
+                {
+                    i = indexStack.Pop();
+                    transitionsArray = stateArrayStack.Pop();
+                }
+
+            }
+            return transitions;
+        }
+
+        ////NOT WORKING 
+        //public ISet<State> GetEpsilonTransitionsRecursive()
+        //{
+        //    var transitions = new SortedSet<State>();
+        //    if(!Transitions.ContainsKey("$")) return transitions;
+        //    foreach (var state in Transitions["$"])
+        //    {
+        //        transitions.Add(state);
+        //        transitions.UnionWith(state.GetEpsilonTransitionsRecursive());
+        //    }
+        //    return transitions;
+        //}
+
+        public int CompareTo(State other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+            return this.Name.CompareTo(other.Name);
+        }
+
+        public bool Equals(State other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+            return this.Name.Equals(other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var state = obj as State;
+            return state != null && this.Equals(state);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name?.GetHashCode() ?? 0;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
